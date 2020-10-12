eb88588cb7307fae824505cdec66a2c35ffda255
ron1502 <ruvelasq@uark.edu>
2020-10-12 15:18:28 -0500

    Drawing process reorganized

--- public/map.js
+++ public/map.js
@@ -1,28 +1,3 @@
-let map;
-
-let RED = "#FF0000";
-let YELLOW = "#FFFF00";
-let GREEN = "#00CC00";
-let BLACK = "#000000";
-let BLUE = "#1A73E8";
-
-const infoFormat = `<div>
-                    <h1>{title}</h1>
-                    <p>Capacity: {capacity}</p>
-                    <p>Available: {available}</p>
-                  </div>`;
-
-function parkingColor(capacity, occupied){
-  occupancy = (occupied)/capacity;
-  if(occupancy >= 0.7) return RED;
-  else if(occupancy >= 0.5 ) return YELLOW;
-  else return GREEN;
-}
-
-/*
-**Draw a svg image
-**Don't update map center
-*/
 
 function centerMap(map, user){
   var coords = user.getPosition();
@@ -45,51 +20,6 @@ function updateLocation(map, user){
   }
 }
 
-
-function drawParkingLot(coordinates, capacity, occupated, size, map){
-  var color = parkingColor(capacity, occupated);
-  var parkingDraw = new google.maps.Circle({
-    strokeColor: color,
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: color,
-    fillOpacity: 0.35,
-    center: coordinates,
-    radius: size
-  });
-  parkingDraw.setMap(map);
-  return parkingDraw;
-}
-
-function drawUserLocation(defaultCoords){
-  var marker = new google.maps.Marker({
-  position: defaultCoords,
-  icon: {
-    path: google.maps.SymbolPath.CIRCLE,
-    strokeColor: BLUE,
-    scale: 5
-  },
-  map: map,
-  });
-  return marker;
-}
-
-function addInfo(pLotArea, info){
-  var infoStr = infoFormat.toString();
-
-  for(const entry in info){
-    infoStr = infoStr.replace(`{${entry}}`, info[entry]);
-  }
-  var infoWindow = new google.maps.InfoWindow({
-    content: infoStr,
-    position: pLotArea.center
-  });
-
-  google.maps.event.addListener(pLotArea, 'click', function(){
-    infoWindow.open(pLotArea.map);
-  });
-}
-
 function initMap(){
   var defaultCoords = {lat: 36.0686895, lng: -94.1748471};
   const noPOI = [{
@@ -97,13 +27,13 @@ function initMap(){
     elementType: "labels",
     stylers: [{ visibility: "off"}]
   }];
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: defaultCoords,
     zoom: 16,
     clickableIcons: false,
     styles: noPOI
   });
-  var userMarker = drawUserLocation(defaultCoords);
+  var userMarker = drawUserLocation(defaultCoords, map);
   updateLocation(map, userMarker);
   centerMap(map, userMarker);
   initTestData(drawParkingLot, addInfo, map);
